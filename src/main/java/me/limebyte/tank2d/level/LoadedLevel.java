eecd2fe5d7d5bcda2af83b6025440357547c6c92
LimeByte <limebyte@twf.co.nz>
2012-11-20 21:02:20 +1300

    Started on levels.

---
+++ src/main/java/me/limebyte/tank2d/level/LoadedLevel.java
@@ -0,0 +1,29 @@
+package me.limebyte.tank2d.level;
+
+import me.limebyte.tank2d.graphics.Screen;
+
+public class LoadedLevel extends Level {
+
+    public LoadedLevel() {
+        super();
+        generateLevel();
+    }
+
+    @Override
+    protected void generateLevel() {
+
+    }
+
+    @Override
+    public void update() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void render(int xScroll, int yScroll, Screen screen) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
