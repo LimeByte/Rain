1b0dd3dbec2f56ac74242d06882476b6581d064d
LimeByte <limebyte@twf.co.nz>
2013-01-09 22:41:41 +1300

    Added a temporary icon.

--- src/main/java/me/limebyte/rain/Main.java
+++ src/main/java/me/limebyte/rain/Main.java
@@ -1,13 +1,17 @@
 package me.limebyte.rain;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class Main {
+
+    private static final ImageIcon ICON = new ImageIcon(Main.class.getResource("/icon.png"));
 
     public static void main(String[] args) {
         Game game = new Game();
 
         game.frame.setTitle(Game.NAME);
+        game.frame.setIconImage(ICON.getImage());
         game.frame.add(game);
         game.frame.pack();
         game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -18,5 +22,4 @@ public class Main {
 
         game.start();
     }
-
 }
